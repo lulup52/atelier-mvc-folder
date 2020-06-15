12c8f1ff9e4ff85b2883461d92c35ea250540cc0
lucas <lucas-p52@hotmail.fr>
2020-06-15 15:33:13 +0200

    errors fixed

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+ res.send('Hello World!')
+});
+
+app.use('/', router);
+
+app.listen(3000, () => {
+ console.log('Example app listening on port 3000!')
+});
+
